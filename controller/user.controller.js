0e3b0c5d6907861bfd0033b0be83b7390b27dea7
aurora-community9 <canvapro2221@gmail.com>
2025-12-22 15:15:09 +0600

    Login api done

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -20,3 +20,21 @@ export const createUser = async (req, res) => {
         res.status(500).json({ success: false, message: "Something went wrong while creating user!" })
     }
 }
+
+export const loginUser = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+        if(!email || !password) return  res.status(400).json({ success: false, message: "Email and password are required!" });
+
+        const user = await User.findOne({email});
+        if(!user) return  res.status(404).json({ success: false, message: "User not found!" });
+
+        const matchPassword = await user.comparePassword(password);
+        if(!matchPassword) return  res.status(400).json({ success: false, message: "Incorrect password!" });
+
+        return  res.status(200).json({ success: true, message: "User login successfully!", user });
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ success: false, message: "Something went wrong while login user!" })
+    }
+}
